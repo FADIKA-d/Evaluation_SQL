c41b1c97e851887b83c6ce2bc295879d96907da9
80010-70-05 <80010-70-05@exchange.ad.afpanet>
2020-02-13 09:09:36 +0100

    Requetes Evaluation SQL

--- EvaluationRequetes.sql
+++ EvaluationRequetes.sql
@@ -1,63 +1,63 @@
 /*1-	Liste des contacts français */
-SELECT CompanyName AS 'Société', ContactName AS 'contact', ContactTitle AS 'Fonction', Phone AS 'Téléphone' FROM customers /*Lire Société, contact, Fonction, Téléphone de la table customers*/
-WHERE Country = 'France' ;
+SELECT CompanyName AS 'Société', ContactName AS 'contact', ContactTitle AS 'Fonction', Phone AS 'Téléphone' FROM customers /*Lire les colonnes CompanyName, ContactName, ContactTitle, Phone de la table customers*/
+WHERE Country = 'France' ; /*Extraction des lignes qui respectent la condition "Country = 'France'"*/
 
 
 /*2 - Produits vendus par le fournisseur « Exotic Liquids » */
-SELECT ProductName AS 'Produit', UnitPrice AS 'Prix' FROM products /*Lire la colonne ProductName des tables products, suppliers */
-JOIN suppliers ON products.SupplierID=suppliers.SupplierID
-WHERE CompanyName = 'Exotic Liquids' ;
+SELECT ProductName AS 'Produit', UnitPrice AS 'Prix' FROM products /*Lire la colonne ProductName de la table products */
+JOIN suppliers ON products.SupplierID=suppliers.SupplierID /* Association de la table suppliers*/
+WHERE CompanyName = 'Exotic Liquids' ; /*Extraction des lignes qui respectent la condition "CompanyName = 'Exotic Liquids' "*/
 
 /*3 - Nombre de produits vendus par les fournisseurs Français dans l’ordre décroissant */
-SELECT CompanyName AS 'Fournisseur', COUNT(ProductID) AS 'Nbre_produits' FROM suppliers /*Lire les colonnes des tables products, suppliers */
-JOIN products ON products.SupplierID=suppliers.SupplierID
-WHERE Country = 'France'
-GROUP BY CompanyName
-ORDER BY Nbre_produits DESC;
+SELECT CompanyName AS 'Fournisseur', COUNT(ProductID) AS 'Nbre_produits' FROM suppliers /*Lire le colonne "CompanyName" et le compte de la colonne "ProductID" de la table suppliers */
+JOIN products ON products.SupplierID=suppliers.SupplierID /* Association de la table products*/
+WHERE Country = 'France' /*Extraction des lignes qui respectent la condition "Country = 'France'"*/
+GROUP BY CompanyName /*Regroupement par "CompanyName "*/
+ORDER BY Nbre_produits DESC; /*Trie de "Nbre_produits" par ordre descendant*/
 
 
 /*4 - Liste des clients Français ayant plus de 10 commandes */
-SELECT CompanyName AS 'Client', COUNT(OrderID) AS 'Nbre_commandes' FROM customers /*Lire les colonnes des tables orders, customers */
-JOIN orders ON customers.CustomerID=orders.CustomerID
-WHERE Country = 'France'
-GROUP BY CompanyName
-HAVING COUNT(OrderID)>10 ;
+SELECT CompanyName AS 'Client', COUNT(OrderID) AS 'Nbre_commandes' FROM customers /*Lire la colonne "CompanyName" et le compte de la colonne "OrderID" de la table customers */
+JOIN orders ON customers.CustomerID=orders.CustomerID /* Association de la table orders*/
+WHERE Country = 'France' /*Extraction des lignes qui respectent la condition "Country = 'France'"*/
+GROUP BY CompanyName /*Regroupement par "CompanyName"*/
+HAVING COUNT(OrderID)>10 ; /*Filtre avec la fonction COUNT la colonne "OrderID" supérieur à 10*/
 
 /*5 - Liste des clients ayant un chiffre d’affaires > 30.000 */
-SELECT CompanyName AS 'Client', SUM(UnitPrice*Quantity) AS 'CA', Country AS 'Pays' FROM `order details`AS orderDetails /*Lire la colonne CompanyName et la somme des tables oders détails,orders, customers  */
-JOIN orders ON orderDetails.OrderID=orders.OrderID
-JOIN customers ON customers.CustomerID=orders.CustomerID
-GROUP BY CompanyName
-HAVING CA>30000.000
-ORDER BY CA DESC;
+SELECT CompanyName AS 'Client', SUM(UnitPrice*Quantity) AS 'CA', Country AS 'Pays' FROM `order details`AS orderDetails /*Lire la colonne "CompanyName" et la somme de la table oders détails */
+JOIN orders ON orderDetails.OrderID=orders.OrderID /* Association de la table orders*/
+JOIN customers ON customers.CustomerID=orders.CustomerID /* Association de la table customers*/
+GROUP BY CompanyName /*Regroupement par "CompanyName"*/
+HAVING CA>30000.000 /*Filtre du "CA" supérieur à 30000000*/
+ORDER BY CA DESC; /*Trie de "CA" par ordre descendant*/
 
-/*6 – Liste des pays dont les clients ont passé commande de produits fournis par « Exotic Liquids » */
-SELECT customers.Country FROM customers /*Lire les colonnes pays des tables customers, products, oders détails, oders */
-JOIN orders ON customers.CustomerID=orders.CustomerID
-JOIN `order details`AS orderDetails ON orderDetails.OrderID=orders.OrderID
-JOIN products ON orderDetails.ProductID=products.ProductID
-JOIN suppliers ON products.SupplierID=suppliers.SupplierID
-WHERE suppliers.CompanyName = 'Exotic Liquids'
-GROUP BY Country
-ORDER BY Country ASC;
+/*6 – Liste des pays dont les clients ont passé commande de produits fournis par "Exotic Liquids" */
+SELECT customers.Country FROM customers /*Lire les colonnes "Country" de la table customers */
+JOIN orders ON customers.CustomerID=orders.CustomerID /* Association de la table orders*/
+JOIN `order details`AS orderDetails ON orderDetails.OrderID=orders.OrderID /* Association de la table orders details*/
+JOIN products ON orderDetails.ProductID=products.ProductID /* Association de la table products*/
+JOIN suppliers ON products.SupplierID=suppliers.SupplierID /* Association de la table suppliers*/
+WHERE suppliers.CompanyName = 'Exotic Liquids' /*Extraction des lignes qui respectent la condition "CompanyName = 'Exotic Liquids'"*/
+GROUP BY Country /*Regroupement par "Country"*/
+ORDER BY Country ASC; /*Trie de "Country" par ordre ascendant*/
 
 /*7 – Montant des ventes de 1997 */
 SELECT SUM(UnitPrice*Quantity) AS 'Montant Ventes 97' FROM `order details` AS orderDetails /*Lire la somme de la table oders détails */
-JOIN orders ON orderDetails.OrderID=orders.OrderID
-WHERE (SUBSTR(OrderDate, 1, 4)) ='1997';
+JOIN orders ON orderDetails.OrderID=orders.OrderID /* Association de la table orders*/
+WHERE (SUBSTR(OrderDate, 1, 4)) ='1997'; /*Extraction des lignes qui respectent la condition 
 
 
 /*8 – Montant des ventes de 1997 mois par mois */
 SELECT MONTH(SUBSTR(OrderDate, 1, 10)) AS 'Mois', SUM(UnitPrice*Quantity) AS 'Montant Ventes' FROM `order details` AS orderDetails /*Lire la somme et les mois de la table oders détails */
-JOIN orders ON orderDetails.OrderID=orders.OrderID
-WHERE (SUBSTR(OrderDate, 1, 4)) ='1997'
-GROUP BY Mois
+JOIN orders ON orderDetails.OrderID=orders.OrderID /* Association de la table orders*/
+WHERE (SUBSTR(OrderDate, 1, 4)) ='1997' /*Extraction des lignes qui respectent la condition ligne "OrderDate" segmenté ='1997'*/
+GROUP BY Mois /*Regroupement par "Mois"*/
 ORDER BY Mois ASC;
 
 /*9 – Depuis quelle date le client « Du monde entier » n’a plus commandé ? */
 SELECT Max(OrderDate) AS 'Date de dernière commande' FROM customers /*Lire le maximum des dates de commandes de la table customers */
-JOIN orders ON customers.CustomerID=orders.CustomerID
-WHERE CompanyName ='Du monde entier'; 
+JOIN orders ON customers.CustomerID=orders.CustomerID /* Association de la table orders*/
+WHERE CompanyName ='Du monde entier';  /*Extraction des lignes qui respectent la condition "CompanyName = 'Du monde entier'"*/
 
 
 /*10 – Quel est le délai moyen de livraison en jours ?*/
